07217fb2ea9f59c952594e866db9f5f298127f12
Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>
2023-08-25 06:46:02 +0530

    Changes 2

--- config/database.js
+++ config/database.js
@@ -3,28 +3,15 @@ require("dotenv").config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
-console.log("PGHOST:", PGHOST);
-console.log("PGDATABASE:", PGDATABASE);
-console.log("PGUSER:", PGUSER);
-
 const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
   host: PGHOST,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // This option might be needed for some environments
     },
   },
 });
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-  });
 
 module.exports = sequelize;
